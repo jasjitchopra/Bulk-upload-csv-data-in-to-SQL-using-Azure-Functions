4980570b8fb1513126db44cab06c726d950c6f8f
jasjitchopra <jasjitchopra@users.noreply.github.com>
2018-11-30 01:00:07 +0530

    Create Customer.sql

---
+++ Code/Customer.sql
@@ -0,0 +1,13 @@
+CREATE TABLE [dbo].[Customer](
+	[CustomerID] [int] NULL,
+	[Title] [nvarchar](50) NULL,
+	[FirstName] [nvarchar](50) NULL,
+	[MiddleName] [nvarchar](50) NULL,
+	[LastName] [nvarchar](50) NULL,
+	[Suffix] [nvarchar](50) NULL,
+	[CompanyName] [nvarchar](50) NULL,
+	[SalesPerson] [nvarchar](50) NULL,
+	[EmailAddress] [nvarchar](50) NULL,
+	[Phone] [nvarchar](50) NULL
+) ON [PRIMARY]
+GO
